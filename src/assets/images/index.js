842c009480c2ff7705b38af24f2ef40f58dcd472
thejavidev <javidation.l@gmail.com>
2023-06-28 21:06:57 +0400

    1d

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,6 +2,8 @@ import logo from './logo.png';
 import banenr from './banner2.jpg';
 import location from './location.png';
 import location2 from './location2.png';
+import user from './user.jpg';
+import bg from './bg.jpg';
 export {
-    logo,banenr,location,location2
+    logo,banenr,location,location2,bg,user
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { LazyLoadImage } from "react-lazy-load-image-component";
-import { banenr,location,location2 } from '../assets/images';
+import { banenr,location,location2, user } from '../assets/images';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const Home = () => {
   return (
     <>
@@ -28,6 +31,34 @@ const Home = () => {
                   <LazyLoadImage src={location2} />
                   <span className='text-[1.2rem] text-[#1b273d] font-[600] capitalize'>Cafe / Restoranlarda görüş</span>
               </div>
+            </div>
+        </div>
+      </section>
+
+      <section className='pt-[70px] pb-[70px] background'>
+        <div className="container mx-auto">
+            <div className="flex items-center justify-center">
+                <h2 className='text-[3.2rem] text-[#fff] font-[800] uppercase'>Qrupun təsisçiləri</h2>
+            </div>
+            <div className="">
+              <Row>
+                <Col lg={4}>
+                  <div className="bg-[#fff] picture-box p-[1.6rem] rounded-[16px] shadow-css-2 relative transitions overflow-hidden">
+                    <figure className='figure relative m-0 '>
+                    <LazyLoadImage src={user} />
+                      <figcaption className='figcaption bg-[--color1] absolute bottom-[0] left-[0] w-[90%] h-[90%] text-center rounded-[8px] opacity-[0] pt-[.8rem] pb-[.8rem] z-[3] transition2'>
+                        <div className="absolute bottom-[5px] z-[99] w-full text-left pt-[1.5rem] pb-[1.5rem] pl-[2rem] pr-[2rem]"></div>
+                      </figcaption>
+                    </figure>
+                  </div>
+                </Col>
+                <Col lg={4}>
+
+                </Col>
+                <Col lg={4}>
+
+                </Col>
+              </Row>
             </div>
         </div>
       </section>
